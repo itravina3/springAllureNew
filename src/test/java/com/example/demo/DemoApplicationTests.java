d2c49f12276d2c3b69175d18ac3ccc89d0e3122b
irynatravina333 <iryna.travina@raiffeisen.ua>
2024-04-25 20:48:00 +0300

    dd

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -7,26 +7,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.demo.pages.MainPage;
 
-@SpringBootTest
-class DemoApplicationTests {
-	//для инъекции классы должны быть помечены аннотацией @Component и быть в одном пакете
-	@Autowired
-	private HomePage homePage;
+//@SpringBootTest
+//class DemoApplicationTests {
+//	//для инъекции классы должны быть помечены аннотацией @Component и быть в одном пакете
+//	@Autowired
+//	private HomePage homePage;
+//
+//	@Autowired
+//	private MainPage mainPage;
+//
+//	//для инъекции значения из application.properties в зависимости от среды (обозначается в конфигурации запуска теста)
+//	@Value("${spring.application.name}")
+//	private String appName;
+//
+//	@Test
+//	void contextLoads() {
+//		homePage.getHomePage();
+//		mainPage.getMainPage();
+//
+//		System.out.println("Application name: " + appName);
+//	}
 
-	@Autowired
-	private MainPage mainPage;
 
-	//для инъекции значения из application.properties в зависимости от среды (обозначается в конфигурации запуска теста)
-	@Value("${spring.application.name}")
-	private String appName;
-
-	@Test
-	void contextLoads() {
-		homePage.getHomePage();
-		mainPage.getMainPage();
-
-		System.out.println("Application name: " + appName);
-	}
-
-
-}
+//}
